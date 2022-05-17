c941872c76f80571349147683deae29def7ed2ae
jgault87 <99287734+jgault87@users.noreply.github.com>
2022-05-17 13:46:53 -0600

    added tech

--- index.js
+++ index.js
@@ -1,79 +1,89 @@
 // TODO: Include packages needed for this application
-const fs = require('fs');
-const inquirer = require('inquirer');
-const generateFile = require('./utils/generateMarkdown.js');
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateFile = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 const promptUser = () => {
-    return inquirer.prompt([
-      {
-        type: 'input',
-        name: 'title',
-        message: 'what is your project name?',
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'please enter a description or userStory for this project',
-      },
-      {
-        type: 'input',
-        name: 'installation',
-        message: 'please provide user setup instructions',
-      },
-      {
-        type: 'input',
-        name: 'usage',
-        message: 'please provide user usage example(s)',
-      },
-      {
-        type: 'input',
-        name: 'contributing',
-        message: 'Who are the contributors of this project? How can some contribute if they wanted to?',
-      },
-      {
-        type: 'input',
-        name: 'username',
-        message: 'please enter your github username',
-      },
-      
-      {
-        type: 'input',
-        name: 'email',
-        message: 'please enter your email',
-      },
-      {
-        type: 'list',
-        name: 'license',
-        message: 'Select the appropriate license for this project',
-        choices: ['MIT', 'Apache', 'GNU']
-      },
-      {
-        type: 'checkbox',
-        message: 'Technologies were used?',
-        name: 'tech',
-        choices: ['HTML5', 'CSS', 'javaScript', 'MySQL', 'Node.js', 'jQuery', 'Bootstrap', 'TailWind', 'WebkitModules', 'Express.js',
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "what is your project name?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "please enter a description or userStory for this project",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "please provide user setup instructions",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "please provide user usage example(s)",
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message:
+        "Who are the contributors of this project? How can some contribute if they wanted to?",
+    },
+    {
+      type: "input",
+      name: "username",
+      message: "please enter your github username",
+    },
+
+    {
+      type: "input",
+      name: "email",
+      message: "please enter your email",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "Select the appropriate license for this project",
+      choices: ["MIT", "Apache", "GNU"],
+    },
+    {
+      type: "checkbox",
+      message: "Technologies were used?",
+      name: "tech",
+      choices: [
+        "HTML5",
+        "CSS",
+        "javaScript",
+        "MySQL",
+        "Node.js",
+        "jQuery",
+        "Bootstrap",
+        "TailWind CSS",
+        "WebkitModules",
+        "Express.js",
+        "Sequelize",
+        "React",
+        "Typescript",
+        "Heroku",
+        "dotEnv",
       ],
-
-      },
-
-
-    ]);
-  };
+    },
+  ]);
+};
 
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 const init = () => {
-promptUser()
-
-.then((data) => fs.writeFileSync('readme.md', generateFile(data)))
-.then(() => console.log('Successfully wrote to readme.md'))
-.catch((err) => console.error(err));
-
-
-}
+  promptUser()
+    .then((data) => fs.writeFileSync("readme.md", generateFile(data)))
+    .then(() => console.log("Successfully wrote to readme.md"))
+    .catch((err) => console.error(err));
+};
 
 // Function call to initialize app
 init();
